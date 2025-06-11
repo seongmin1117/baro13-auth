4c51f0c59a85e841e6f5fb7cce36fa087cc0e05a
seongmin <seongmin1117@gmail.com>
2025-06-11 23:34:28 +0900

    init

---
+++ src/main/java/com/baro13/auth/Baro13AuthApplication.java
@@ -0,0 +1,12 @@
+package com.baro13.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baro13AuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Baro13AuthApplication.class, args);
+  }
+}
---
+++ src/test/java/com/baro13/auth/Baro13AuthApplicationTests.java
@@ -0,0 +1,11 @@
+package com.baro13.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baro13AuthApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
